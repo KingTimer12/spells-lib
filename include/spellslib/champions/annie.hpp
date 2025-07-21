971a56b06a8884c92a6011f3bb700f9385058f4c
Aaron King <queentimer12@gmail.com>
2025-07-21 10:06:49 -0300

    chore: updated Annie

--- include/spellslib/champions/annie.hpp
+++ include/spellslib/champions/annie.hpp
@@ -8,45 +8,33 @@
 class AnnieSpells : public ChampionSpells {
  public:
   Spell get_q() const override {
-    const Spell spell = Spell{
+    return {
         .type = SpellTypeData::TARGETTED,
         .range = 625.f,
         .delay = .25f,
     };
-    return spell;
   }
   Spell get_w() const override {
-    const Spell spell = Spell{
+    return {
         .type = SpellTypeData::CONE,
         .range = 625.f,
         .speed = 1400.f,
         .delay = .25f,
         .angle = 49.52f,
     };
-
-    return spell;
   }
   Spell get_e() const override {
-    const Spell spell = Spell{
+    return {
         .type = SpellTypeData::NONE,
         .range = 800.f,
     };
-    return spell;
   }
   Spell get_r() const override {
-    auto attributes = create_map();
-    attributes["radius"] = []() { return 250.0f; };
-
-    float range = 600.f;
-    float delay = .25f;
-
-    const Spell spell = Spell{
+    return {
         .type = SpellTypeData::CIRCULAR,
         .range = 600.f,
         .delay = .25f,
         .radius = 250.f,
     };
-
-    return spell;
   }
 };
